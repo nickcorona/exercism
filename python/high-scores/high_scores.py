dc7a8d0d0a0fbb5a4f79277a00420f4f4696bafa
Nick Corona <nicklucianocorona@gmail.com>
2019-04-13 09:34:11 -0400

    Tested high_scores.py

--- python/high-scores/high_scores.py
+++ python/high-scores/high_scores.py
@@ -2,26 +2,24 @@ class HighScores(object):
     def __init__(self, scores):
         self.scores = scores
 
-    def highest_score(self):
+    def personal_best(self):
         return max(self.scores)
 
-    def last_score(self):
+    def latest(self):
         return self.scores[-1]
 
-    def three_highest_scores(self):
+    def personal_top_three(self):
         max_scores = []
         scores2 = self.scores.copy()
-        for i in range(0, 3):
-            max_score = max(scores2)
+
+        i = 0
+        while i < 3:
+            try:
+                max_score = max(scores2)
+            except ValueError:
+                break
             max_score_index = scores2.index(max_score)
             scores2.pop(max_score_index)
             max_scores.append(max_score)
+            i += 1
         return max_scores
-
-
-nick = HighScores([30, 50, 20, 70])
-
-nick.highest_score()
-nick.last_score()
-nick.three_highest_scores()
-
