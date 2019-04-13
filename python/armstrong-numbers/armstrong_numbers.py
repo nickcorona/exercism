dec42a5b1ba03d107504ebdbad3cd343817e3b4a
Nick Corona <nicklucianocorona@gmail.com>
2019-04-13 10:49:17 -0400

    variable name change.

--- python/armstrong-numbers/armstrong_numbers.py
+++ python/armstrong-numbers/armstrong_numbers.py
@@ -2,8 +2,8 @@ def is_armstrong(number):
     str_number = str(number)
     length = len(str_number)
     lst = []
-    for integer in str_number:
-        lst.append(int(integer) ** length)
+    for str_integer in str_number:
+        lst.append(int(str_integer) ** length)
     if sum(lst) == number:
         return True
     else:
