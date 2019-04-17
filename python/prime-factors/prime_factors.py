a3c26a9975507c909f7ecc4364fa82590734d674
Nick Corona <nicklucianocorona@gmail.com>
2019-04-16 21:36:05 -0400

    Done prime-factors.

--- python/prime-factors/prime_factors.py
+++ python/prime-factors/prime_factors.py
@@ -1,2 +1,11 @@
 def prime_factors(natural_number):
-    pass
+    number_left = natural_number
+    prime_factors = []
+    divisor = 2
+    while number_left > 1:
+        if number_left % divisor == 0:
+            prime_factors.append(divisor)
+            number_left = number_left / divisor
+        else:
+            divisor += 1
+    return prime_factors
