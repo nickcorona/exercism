3e6e2335f3e5ce7dc243e477c204fc11ce431ef8
Nick Corona <nicklucianocorona@gmail.com>
2019-04-21 23:06:37 -0400

    Commented.

--- python/matrix/matrix.py
+++ python/matrix/matrix.py
@@ -1,8 +1,11 @@
 class Matrix(object):
     def __init__(self, matrix_string):
         self.matrix = []
+        # converts to list of lists
         for string in matrix_string.strip().split("\n"):
             self.matrix.append(string.split())
+
+        # converts strings to numbers
         self.matrix = [
             [int(string) for string in self.matrix[i]] for i in range(len(self.matrix))
         ]
@@ -12,6 +15,8 @@ class Matrix(object):
 
     def column(self, index):
         column_length = len(self.matrix[0])
+
+        # transposes and returns column
         return [[row[i] for row in self.matrix] for i in range(column_length)][
             index - 1
         ]
