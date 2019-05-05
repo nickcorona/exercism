ede15fc89a03546e187501479211803132e123da
Nick Corona <nicklucianocorona@gmail.com>
2019-05-05 13:11:26 -0400

    Added difference-of-squares.

---
+++ ruby/rna-transcription/rna_transcription.rb
@@ -0,0 +1,13 @@
+rna = { "G" => "C", "C" => "G", "T" => "A", "A" => "U" }
+
+class complement
+  def of_dna(string)
+    for letter in string do
+      rna
+  end
+end
+
+# * `G` -> `C`
+# * `C` -> `G`
+# * `T` -> `A`
+# * `A` -> `U`
