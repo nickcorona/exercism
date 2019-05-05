299d9983590d1bb9245e9372584e050c75fc2b84
Nick Corona <nicklucianocorona@gmail.com>
2019-05-05 13:48:12 -0400

    Done anagrams.

--- python/anagram/anagram.py
+++ python/anagram/anagram.py
@@ -1,2 +1,21 @@
 def find_anagrams(word, candidates):
-    pass
+    word_lower = word.lower()
+    word_sorted = "".join(sorted(word_lower))
+
+    # remove redundant words
+    for candidate in candidates[:]:
+        if word_lower == candidate.lower():
+            candidates.remove(candidate)
+
+    # sort candidate words
+    candidates_sorted = []
+    for candidate in candidates:
+        candidates_sorted.append("".join(sorted(candidate.lower())))
+
+    # check if word is in sorted candidates
+    # if so return
+    return_candidates = []
+    for index, candidate in enumerate(candidates_sorted):
+        if candidate == word_sorted:
+            return_candidates.append(candidates[index])
+    return return_candidates
