6e5399aa22dc8ac5af5ba3762707725b90c4cf76
Nick Corona <nicklucianocorona@gmail.com>
2019-05-05 20:47:30 -0400

    Done leap.

--- julia/leap/leap.jl
+++ julia/leap/leap.jl
@@ -1,4 +1,11 @@
 function is_leap_year(year::Int)
-
-end
-
+    if year % 4
+        if year % 100
+            if year % 400
+                return true
+            else
+                return false
+            end
+        end
+    end
+end    
