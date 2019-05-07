8a96fee8136879e95d66a1e4cb297008b0325efc
Nick Corona <nicklucianocorona@gmail.com>
2019-05-06 20:52:46 -0400

    Calculate combinations with replacement.

--- python/palindrome-products/palindrome_products.py
+++ python/palindrome-products/palindrome_products.py
@@ -1,6 +1,18 @@
+from itertools import combinations_with_replacement
+
+
 def largest_palindrome(max_factor, min_factor):
     pass
 
 
 def smallest_palindrome(max_factor, min_factor):
     pass
+
+
+max_factor = 9
+min_factor = 1
+
+combination_lst = combinations_with_replacement(range(min_factor, max_factor + 1), r=2)
+[tup[0] * tup[1] for tup in combination_lst]
+
+
