90aecb5e9a3eb511901293592a64009c5335c7a4
Nick Corona <nicklucianocorona@gmail.com>
2019-05-06 20:14:38 -0400

    Done raindrops.

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,2 +1,13 @@
 def raindrops(number):
-    pass
+    words = []
+    if number % 3 == 0:
+        words.append('Pling')
+    if number % 5 == 0:
+        words.append('Plang')
+    if number % 7 == 0:
+        words.append('Plong')
+
+    if len(words) == 0:
+        return str(number)
+    else:
+        return ''.join(words)
