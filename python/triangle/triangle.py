b56e6d0e1ce10488c044b445cc23b43d1c9e8628
Nick Corona <nicklucianocorona@gmail.com>
2019-05-07 21:07:11 -0400

    Done triangle.

--- python/triangle/triangle.py
+++ python/triangle/triangle.py
@@ -1,10 +1,34 @@
 def is_equilateral(sides):
-    pass
+    sorted_sides = sorted(sides)
+    if sum(sides) == 0:
+        return False
+    if sorted_sides[0] + sorted_sides[1] < sorted_sides[2]:
+        return False
+    if sides[0] == sides[1] == sides[2]:
+        return True
+    else:
+        return False
 
 
 def is_isosceles(sides):
-    pass
+    sides_sorted = sorted(sides)
+    if sum(sides) == 0:
+        return False
+    if sides_sorted[0] + sides_sorted[1] < sides_sorted[2]:
+        return False
+    for side in sides:
+        if sides.count(side) >= 2:
+            return True
+    return False
 
 
 def is_scalene(sides):
-    pass
+    sides_sorted = sorted(sides)
+    if sum(sides) == 0:
+        return False
+    if sides_sorted[0] + sides_sorted[1] < sides_sorted[2]:
+        return False
+    if sides[0] != sides[1] != sides[2]:
+        return True
+    else:
+        return False
