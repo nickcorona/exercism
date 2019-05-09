02c08d07fc79fabd5988b482f7480de420bfe137
Nick Corona <nicklucianocorona@gmail.com>
2019-05-09 18:54:16 -0400

    Added collatz conjecture

--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,2 +1,48 @@
+codon_protein_dict = {
+    "AUG": ["Methionine"],
+    "UUU": ["Phenylalanine"],
+    "UUC": ["Phenylalanine"],
+    "UUA": ["Leucine"],
+    "UUG": ["Leucine"],
+    "UCU": ["Serine"],
+    "UCC": ["Serine"],
+    "UCA": ["Serine"],
+    "UCG": ["Serine"],
+    "UAU": ["Tyrosine"],
+    "UAC": ["Tyrosine"],
+    "UGU": ["Cysteine"],
+    "UGC": ["Cysteine"],
+    "UGG": ["Tryptophan"],
+    "UAA": ["STOP"],
+    "UAG": ["STOP"],
+    "UGA": ["STOP"],
+}
+
+
 def proteins(strand):
-    pass
+    if len(strand) % 3 != 0:
+        return []
+
+    proteins = []
+    for i in range(0, len(strand), 3):
+        proteins += codon_protein_dict[strand[i: i + 3]]
+        if "STOP" in proteins:
+            break
+    return proteins 
+
+proteins('AUGUCG')
+
+# for i in range(0, len('AUGACC'), 3):
+#     print('AUGACC'[i: i + 3])
+
+
+# Codon                 | Protein
+# :---                  | :---
+# AUG                   | Methionine
+# UUU, UUC              | Phenylalanine
+# UUA, UUG              | Leucine
+# UCU, UCC, UCA, UCG    | Serine
+# UAU, UAC              | Tyrosine
+# UGU, UGC              | Cysteine
+# UGG                   | Tryptophan
+# UAA, UAG, UGA         | STOP
