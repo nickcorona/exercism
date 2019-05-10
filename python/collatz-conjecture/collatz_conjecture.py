47b7c855cd587b96a822c1b1a3087f645e9ba543
Nick Corona <nicklucianocorona@gmail.com>
2019-05-09 21:09:52 -0400

    Changed operator.

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -4,7 +4,7 @@ def collatz_steps(number):
     steps = 0
     while number != 1:
         if number % 2 == 0:
-            number = number / 2
+            number /= 2
         else:
             number = number * 3 + 1
         steps += 1
