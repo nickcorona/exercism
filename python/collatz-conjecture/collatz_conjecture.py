aaa085f688f06d3c7cea78a39b61a081dd1715d2
Nick Corona <nicklucianocorona@gmail.com>
2019-05-09 20:59:24 -0400

    Done collatz conjecture.

--- python/collatz-conjecture/collatz_conjecture.py
+++ python/collatz-conjecture/collatz_conjecture.py
@@ -1,2 +1,11 @@
 def collatz_steps(number):
-    pass
+    if number <= 0:
+        raise ValueError("Number must be positive..")
+    steps = 0
+    while number != 1:
+        if number % 2 == 0:
+            number = number / 2
+        else:
+            number = number * 3 + 1
+        steps += 1
+    return steps
