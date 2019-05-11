df7bb664acb07f073dbb6b57b144d589b9b9de74
Nick Corona <nicklucianocorona@gmail.com>
2019-05-11 13:53:17 -0400

    Done resistor-color-duo

--- python/resistor-color-duo/resistor_color_duo.py
+++ python/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,14 @@
 def value(colors):
-    pass
+    color_value_dict = {
+        "black": "0",
+        "brown": "1",
+        "red": "2",
+        "orange": "3",
+        "yellow": "4",
+        "green": "5",
+        "blue": "6",
+        "violet": "7",
+        "grey": "8",
+        "white": "9",
+    }
+    return int(color_value_dict.get(colors[0]) + color_value_dict.get(colors[1]))
--- python/simple-cipher/simple_cipher.py
+++ python/simple-cipher/simple_cipher.py
@@ -7,35 +7,3 @@ class Cipher(object):
 
     def decode(self, text):
         pass
-
-
-string = 'balls'
-
-shift = -1
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
-shifted_indices = [alphabet.index(char) + shift for char in string]
-
-encoded_string = ''.join([alphabet[index] for index in shifted_indices])
-print(encoded_string)
-
-encoded_string = 'azkkr'
-
-shift = 1
-
-indices = [alphabet.index(char) - shift for char in encoded_string]
-
-decoded_string = ''.join([alphabet[index] for index in indices])
-print(decoded_string)
-
-###
-
-string = 'balls'
-key = 'dddd'
-
-shift_lst = [alphabet.index(letter) for letter in key]
-
-counter = 0
-letter_lst = []
-while counter < len(string):
-    alphabet[shift_lst[0]]
