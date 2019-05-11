a504ca70d0283690e01e25f4698c96707db43a51
Nick Corona <nicklucianocorona@gmail.com>
2019-05-11 13:34:35 -0400

    Done resistor-color.

--- python/resistor-color/resistor_color.py
+++ python/resistor-color/resistor_color.py
@@ -1,6 +1,19 @@
 def color_code(color):
-    pass
+    color_num_dict = dict(zip(colors(), range(0, len(colors()))))
+    return color_num_dict[color]
 
 
 def colors():
-    pass
+    colors = [
+        "black",
+        "brown",
+        "red",
+        "orange",
+        "yellow",
+        "green",
+        "blue",
+        "violet",
+        "grey",
+        "white",
+    ]
+    return colors
