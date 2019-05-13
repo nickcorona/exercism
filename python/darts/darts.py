707d6afd747c26658a16a2ebd9f31bebe8c94d1d
Nick Corona <nicklucianocorona@gmail.com>
2019-05-12 20:05:18 -0400

    1st iteration of protein-translation.

--- python/darts/darts.py
+++ python/darts/darts.py
@@ -1,2 +1,10 @@
 def score(x, y):
-    pass
+    form = (x ** 2 + y ** 2) ** (1 / 2)
+    if form <= 1:
+        return 10
+    elif form <= 5:
+        return 5
+    elif form <= 10:
+        return 1
+    else:
+        return 0
--- python/protein-translation/protein_translation.py
+++ python/protein-translation/protein_translation.py
@@ -1,48 +1,33 @@
-codon_protein_dict = {
-    "AUG": ["Methionine"],
-    "UUU": ["Phenylalanine"],
-    "UUC": ["Phenylalanine"],
-    "UUA": ["Leucine"],
-    "UUG": ["Leucine"],
-    "UCU": ["Serine"],
-    "UCC": ["Serine"],
-    "UCA": ["Serine"],
-    "UCG": ["Serine"],
-    "UAU": ["Tyrosine"],
-    "UAC": ["Tyrosine"],
-    "UGU": ["Cysteine"],
-    "UGC": ["Cysteine"],
-    "UGG": ["Tryptophan"],
-    "UAA": ["STOP"],
-    "UAG": ["STOP"],
-    "UGA": ["STOP"],
-}
-
-
 def proteins(strand):
-    if len(strand) % 3 != 0:
-        return []
+    codons = []
+    for i in range(0, len(strand), 3):
+        codons.append(strand[i : i + 3])
 
     proteins = []
-    for i in range(0, len(strand), 3):
-        proteins += codon_protein_dict[strand[i: i + 3]]
-        if "STOP" in proteins:
+    for codon in codons:
+        if codon_dict()[codon] == "STOP":
             break
-    return proteins 
-
-proteins('AUGUCG')
-
-# for i in range(0, len('AUGACC'), 3):
-#     print('AUGACC'[i: i + 3])
+        proteins.append(codon_dict()[codon])
+    return proteins
 
 
-# Codon                 | Protein
-# :---                  | :---
-# AUG                   | Methionine
-# UUU, UUC              | Phenylalanine
-# UUA, UUG              | Leucine
-# UCU, UCC, UCA, UCG    | Serine
-# UAU, UAC              | Tyrosine
-# UGU, UGC              | Cysteine
-# UGG                   | Tryptophan
-# UAA, UAG, UGA         | STOP
+def codon_dict():
+    return {
+        "AUG": "Methionine",
+        "UUU": "Phenylalanine",
+        "UUC": "Phenylalanine",
+        "UUA": "Leucine",
+        "UUG": "Leucine",
+        "UCU": "Serine",
+        "UCC": "Serine",
+        "UCA": "Serine",
+        "UCG": "Serine",
+        "UAU": "Tyrosine",
+        "UAC": "Tyrosine",
+        "UGU": "Cysteine",
+        "UGC": "Cysteine",
+        "UGG": "Tryptophan",
+        "UAA": "STOP",
+        "UAG": "STOP",
+        "UGA": "STOP",
+    }
