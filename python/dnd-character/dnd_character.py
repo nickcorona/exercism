7b52eb02cbc341ce0fbfd1f92e5dc7d7f2eaafc9
Nick Corona <nicklucianocorona@gmail.com>
2019-05-19 23:46:22 -0400

    Done dnd-character.

--- python/dnd-character/dnd_character.py
+++ python/dnd-character/dnd_character.py
@@ -1,3 +1,20 @@
+from random import choices
+from math import floor
+
+
 class Character:
     def __init__(self):
-        pass
+        self.strength = self.ability()
+        self.dexterity = self.ability()
+        self.constitution = self.ability()
+        self.intelligence = self.ability()
+        self.wisdom = self.ability()
+        self.charisma = self.ability()
+        self.hitpoints = 10 + modifier(self.constitution)
+
+    def ability(self):
+        return sum(sorted(choices(population=range(1, 7), k=4)[1:4]))
+
+
+def modifier(constitution):
+    return floor((constitution - 10) / 2)
