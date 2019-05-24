2089eb0e468beefcfe8012802c15421d297e4955
Nick Corona <nicklucianocorona@gmail.com>
2019-05-23 23:20:56 -0400

    replaced for-loop with list comprehension

--- python/raindrops/raindrops.py
+++ python/raindrops/raindrops.py
@@ -1,9 +1,5 @@
 def raindrops(number):
     factors = (3, 5, 7)
     sounds = ("Pling", "Plang", "Plong")
-    strings = []
-
-    for factor, sound in zip(factors, sounds):
-        if number % factor == 0:
-            strings.append(sound)
+    strings = [sound for factor, sound in zip(factors, sounds) if number % factor == 0]
     return "".join(strings) or str(number)
