1405aee883e10412855fda42f0949f69a14f19e9
Nick Corona <nicklucianocorona@gmail.com>
2019-05-27 20:51:46 -0400

    1st iteration

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -7,9 +7,6 @@ package twofer
 
 // ShareWith should have a comment documenting it.
 func ShareWith(name string) string {
-	// Write some code here to pass the test suite.
-	// Then remove all the stock comments.
-	// They're here to help you get started but they only clutter a finished solution.
-	// If you leave them in, reviewers may protest!
+	if name 
 	return ""
 }
---
+++ teams/data-science/python/collatz-conjecture/collatz_conjecture.py
@@ -0,0 +1,11 @@
+def steps(number):
+    if number < 1:
+        raise ValueError("Number must be greater than 0.")
+    steps = 0
+    while number != 1:
+        steps += 1
+        if number % 2 == 0:
+            number /= 2
+        else:
+            number = 3 * number + 1
+    return steps
